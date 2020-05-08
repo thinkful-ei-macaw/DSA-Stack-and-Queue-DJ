76be589a6fc83e8611076de0b5c09f205f866d76
Jordan <jordanknox@Jordans-MacBook-Pro-4.local>
2020-05-08 11:16:50 -0700

    completed 1-4

--- SQDrills.js
+++ SQDrills.js
@@ -47,11 +47,59 @@ function display(stack) {
   let node = stack.top;
 
   while (node.next !== null) {
-    console.log(node.data)
+    console.log(node.data);
     node = node.next;
   }
-  console.log(node.data)
+  console.log(node.data);
   return;
+}
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let stack = new Stack;
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s[i]);
+  }
+  let douglas = '';
+  for (let i = 0; i < s.length; i++) {
+    douglas += stack.pop();
+  }
+  if (douglas === s) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+}
+
+function parens(exp) {
+  let stack = new Stack;
+  let count = 0;
+  let arr = exp.match(/[()]/g);
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i]);
+  }
+  if (stack.top === null) {
+    return;
+  }
+  for(let i =0; i < arr.length; i++) {
+    let value = stack.pop();
+
+    if(value === '(') {
+      count++;
+    } else {
+      count--;
+    } 
+  }
+ 
+  if (count === 0 ) {
+    return true;
+  } else if (count > 0) {
+    return 'You are missing a ")"';
+  } else {
+    return 'You are missing a "("';
+  }
 }
 
 function main() {
@@ -75,8 +123,13 @@ function main() {
   starTrek.push('Scotty');
 
   display(starTrek);
-
+ 
   return starTrek;
 }
 
 console.log(main());
+is_palindrome('dad');
+is_palindrome('A man, a plan, a canal: Panama');
+is_palindrome('1001');
+is_palindrome('Tauhida');
+console.log(parens('()()()('));
